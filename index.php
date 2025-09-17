544eca59f5ec79a9f31f68a49ba82c76c9bc18ba
gonnmerino <gonnmerino@gmail.com>
2025-09-16 23:49:20 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link href="./src/output.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <div className="container min-h-screen min-w-screen bg-black">
+    </div>
+</body>
+</html>
+
+?>
