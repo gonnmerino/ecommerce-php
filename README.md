89384e698d0def1674a6aab04900bfe3b59e6840
gonnmerino <gonnmerino@gmail.com>
2025-12-10 20:27:45 -0300

    Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Proyecto final de primer año – PHP + SQL
+Ecommerce de hardware componentes y afines con **ABM de productos, stock y panel de administracion con permisos de usuarios.**
+-En la carpeta hay dos archivos SQL, uno con las tablas que se crean vacias y el segundo archivo tambien crea las tablas pero con datos
+para poder probar el proyecto.
+
+Gracias por pasar.
--- src/includes/header.php
+++ src/includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>ECOMMERCE - Fuente Universal de Componentes</title>
-    <link rel="icon" type="image/x-icon" href="favicon.png">
+    <link rel="icon" type="image/x-icon" href="/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <link href="./src/output.css" rel="stylesheet">
